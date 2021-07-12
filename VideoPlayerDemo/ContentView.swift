f08eb3638f06b32814a5a293c856381d1bf3b126
Demo <Demo>
2021-07-12 17:30:27 -0400

    Initial Commit

---
+++ VideoPlayerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayerDemo
+//
+//  Created by Standard on 7/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
